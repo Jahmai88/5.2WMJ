aadc442eb1cc0a913961bb9184418fc6ec27c236
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 12:55:33 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 12;
+        int rating = 5;
+        if (rating >=5 || ticket < 12);
+        System.out.println("Im interested in watching this movie");
+
+         //else
+        System.out.println("I am not interested in watching this movie");
+        //endif
+        //end method main
+    }
+}
